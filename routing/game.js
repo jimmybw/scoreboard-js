949931bd5529fb0be4ef75723031829ac88f2303
Chris Skilton <chriss@brandwatch.com>
2014-07-24 21:01:27 +0100

    added basic authentication of requests

--- routing/game.js
+++ routing/game.js
@@ -5,6 +5,8 @@ var express= require('express'),
     Game = require('../js/game'),
     socketio = require('socket.io');
 
+var AUTH_TOKEN = 'Basic thefooser';
+
 function returnGameStateAndBroadcastState(io, res, game){
     var gameState = game.getGameState();
 
@@ -16,6 +18,17 @@ function emitUpdatedGameState(io, gameState){
     io.sockets.emit('update', gameState);
 }
 
+
+function authenticate(req, res, next){
+    var auth = req.headers.authorization;
+
+    if(auth !== AUTH_TOKEN){
+        return res.send('incorrect authentication');
+    }
+
+    next();
+}
+
 module.exports = function(socket){
     var router = new Router(),
         io = socket;
@@ -23,6 +36,9 @@ module.exports = function(socket){
     io.sockets.on('connection', function(){
         emitUpdatedGameState(io, Game.getGame().getGameState());
     });
+
+    router.get('*', authenticate);
+    router.post('*', authenticate);
 
     /*
         LOGS A GOAL TO THE GAME
